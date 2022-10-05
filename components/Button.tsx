Chrizpy/hello-world

commit af5745a97179bf5bc64d01d394c3657235b92d69

Add a collapse button and autogenerate links
A lot of stuff happens in this commit since I forgot to checkout
changes as I went.

* DRY:ed up Navigation by using a constant array of routes,
from this array I generate links and anchors.

* Add a collapse button. This uses the island architecture, so that
the collapsing is done on client side.

components/Button.tsx
@@ -1,0 +1,22 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+
+interface ButtonProps {
+  class: string;
+  children: string;
+}
+
+const buttonStyling =
+  "block sm:hidden bg-banner rounded px-2 border border-black";
+
+export default function Button(props: ButtonProps): h.JSX.Element {
+  return (
+    <button
+      {...props}
+      class={tw`${buttonStyling}`}
+    >
+      {props.children}
+    </button>
+  );
+}

components/Navigation.tsx
@@ -2,70 +2,65 @@
 import { h } from "preact";
 import { tw } from "@twind";
 
-const anchorStyling = "p-1 hover:bg-banner rounded-lg transition-all ease-in-out";
+import CollapseButton from "../islands/CollapseButton.tsx";
 
-export default function Navigation() {
+interface Route {
+  path: string;
+  value: string;
+}
+
+function formatRoute(route: string): string {
+  if (route === "/") {
+    return "Home";
+  } else {
+    route = route.replace("-", " ")
+      .replace("/", "")
+      .toLowerCase();
+
+    return route.charAt(0).toUpperCase() + route.slice(1);
+  }
+}
+
+function initiateRoutes(routes: string[]): Route[] {
+  return routes.map((route) => ({
+    path: route,
+    value: formatRoute(route),
+  }));
+}
+
+const anchorStyling =
+  "p-1 hover:bg-banner rounded-lg transition-all ease-in-out";
+const routes = [
+  "/",
+  "/about",
+  "/tech-used",
+  "/cv",
+  "/my-projects",
+  "/open-source",
+  "conferences",
+];
+
+function createLinks(): h.JSX.Element[] {
+  return initiateRoutes(routes).map((route) => (
+    <li>
+      <a
+        class={tw`${anchorStyling}`}
+        href={route.path}
+      >
+        {route.value}
+      </a>
+    </li>
+  ));
+}
+
+export default function Navigation(): h.JSX.Element {
   return (
     <div class={tw`text-center text-2xl content-center `}>
-      <nav>
+      <CollapseButton target="navCollapse" />
+
+      <nav class={tw`hidden sm:block`} id="navCollapse">
         <ul>
-          <li>
-            <a
-              href="/"
-              class={tw`${anchorStyling}`}
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href="/about"
-              class={tw`${anchorStyling}`}
-            >
-              About
-            </a>
-          </li>
-          <li>
-            <a
-              href="/tech-used"
-              class={tw`${anchorStyling}`}
-            >
-              Tech used
-            </a>
-          </li>
-          <li>
-            <a
-              href="/cv"
-              class={tw`${anchorStyling}`}
-            >
-              CV
-            </a>
-          </li>
-          <li>---</li>
-          <li>
-            <a
-              href="/my-projects"
-              class={tw`${anchorStyling}`}
-            >
-              My projects
-            </a>
-          </li>
-          <li>
-            <a
-              href="/open-source"
-              class={tw`${anchorStyling}`}
-            >
-              Open-source contributions
-            </a>
-          </li>
-          <li>
-            <a
-              href="/conferences"
-              class={tw`${anchorStyling}`}
-            >
-              Conferences I have attended
-            </a>
-          </li>
+          {createLinks()}
         </ul>
       </nav>
     </div>

fresh.gen.ts
@@ -9,6 +9,7 @@ import * as $3 from "./routes/index.tsx";
 import * as $4 from "./routes/my-projects.tsx";
 import * as $5 from "./routes/open-source.tsx";
 import * as $6 from "./routes/tech-used.tsx";
+import * as $$0 from "./islands/CollapseButton.tsx";
 
 const manifest = {
   routes: {
@@ -20,7 +21,9 @@ const manifest = {
     "./routes/open-source.tsx": $5,
     "./routes/tech-used.tsx": $6,
   },
-  islands: {},
+  islands: {
+    "./islands/CollapseButton.tsx": $$0,
+  },
   baseUrl: import.meta.url,
 };
 

islands/CollapseButton.tsx
@@ -1,0 +1,43 @@
+/** @jsx h */
+import { h } from "preact";
+import { useState } from "preact/hooks";
+
+import Button from "../components/Button.tsx";
+
+interface CollapseButtonProps {
+  target: string;
+}
+
+function manipulateTarget(target: string, state: boolean) {
+  const targetElement = document.getElementById(target);
+  const targetElementClasses = targetElement?.classList;
+
+  if (state) {
+    targetElementClasses?.add("block");
+    targetElementClasses?.remove("hidden");
+  } else {
+    targetElementClasses?.add("hidden");
+    targetElementClasses?.remove("block");
+  }
+}
+
+export default function CollapseButton(
+  props: CollapseButtonProps,
+): h.JSX.Element {
+  const [state, setState] = useState(true);
+  const switchCollapseState = () => {
+    setState(!state);
+
+    manipulateTarget(props.target, state);
+  };
+
+  return (
+    <div>
+      <Button
+        onClick={() => switchCollapseState()}
+      >
+        . . .
+      </Button>
+    </div>
+  );
+}

commit bb1211da491a63821a2ec075e6493ac47dc7ae15

Fix `error: TS2322 [ERROR]:`
Introduced in af5745a.

Basically, what I need to do is to clean up my props interface,
it did not include the correct attributes.

error: TS2322 [ERROR]: Type '{ children: string; onClick: () => void; }' is not assignable to type 'IntrinsicAttributes & ButtonProps'.
  Property 'onClick' does not exist on type 'IntrinsicAttributes & ButtonProps'.
        onClick={() => switchCollapseState()}
        ~~~~~~~
    at file:///home/christoffer/personal/hello-world/islands/CollapseButton.tsx:37:9

components/Button.tsx
@@ -3,7 +3,7 @@ import { h } from "preact";
 import { tw } from "@twind";
 
 interface ButtonProps {
-  class: string;
+  onClick: h.JSX.MouseEventHandler<HTMLButtonElement>;
   children: string;
 }
 