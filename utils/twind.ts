696fe5c69242145b93f49b07ef1a0440dd01ac77
Chris <38702971+Chrizpy@users.noreply.github.com>
2022-10-08 16:23:23 +0200

    Remove use of old `bg-dotted-background` class
    Moving away from this in favour of 5353f36.
    
    The reason was that the background didn't completely cover
    the whole background of the page before.
    
    This was noticed especially on IOS devices. Not really on my
    computers web browser.

--- utils/twind.ts
+++ utils/twind.ts
@@ -2,7 +2,6 @@ import { IS_BROWSER } from "$fresh/runtime.ts";
 import { Configuration, setup } from "twind";
 export * from "twind";
 
-const dottedBackground = "url(/svg/small_dot.svg)";
 const nanumPenScriptImport =
   `url('https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap')`;
 
@@ -11,9 +10,6 @@ export const config: Configuration = {
   mode: "silent",
   theme: {
     extend: {
-      backgroundImage: {
-        "dotted-background": dottedBackground,
-      },
       colors: {
         "banner": "#B5F9F1",
       },
