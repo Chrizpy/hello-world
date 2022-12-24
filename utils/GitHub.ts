5e4b673a60337505373b0ef86ceb1ed25de7767a
Chris <38702971+Chrizpy@users.noreply.github.com>
2022-12-24 15:34:18 +0100

    Add content to `/open-source`
    Populate the `/open-source` page a little by querying the GitHub API.
    Showing the pull requests I have done in public repos.

---
+++ utils/GitHub.ts
@@ -0,0 +1,17 @@
+export interface PullRequest {
+  url: string;
+  html_url: string;
+  merged_at: string;
+}
+
+export interface GitHubData {
+  url: string;
+  repository_url: string;
+  title: string;
+  state: string;
+  pull_request: PullRequest;
+}
+
+const user = Deno.env.get("USER")
+
+export const GitHubAPIQuery = `%20is:pull-request%20author:${user}%20archived:false%20is:public%20-user:${user}&per_page=100&page=1`
