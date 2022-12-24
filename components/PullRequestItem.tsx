7a3a63950f3c126263439a257a9eda7e0c2506f0
Chris <38702971+Chrizpy@users.noreply.github.com>
2022-12-24 15:51:12 +0100

    Add open source content (#8)
    * Automated changes
    
    Throwing it into this branch because I am lazy and nobody else will
    probably see this anyway.
    
    inb4 I lose a potential job oppurtinity. 🥹
    
    * Add content to `/open-source`
    
    Populate the `/open-source` page a little by querying the GitHub API.
    Showing the pull requests I have done in public repos.

---
+++ components/PullRequestItem.tsx
@@ -0,0 +1,29 @@
+import IconGitPullRequest from "https://deno.land/x/tabler_icons_tsx@0.0.2/tsx/git-pull-request.tsx";
+import IconGitMerge from "https://deno.land/x/tabler_icons_tsx@0.0.2/tsx/git-merge.tsx";
+
+import { GitHubData } from "../utils/GitHub.ts"
+
+function pullRequestIcon(state: string) {
+  if (state === "open") return <IconGitPullRequest />;
+  if (state === "closed") {
+    return <IconGitMerge class="inline text-purple-500" />;
+  }
+}
+
+export default function PullRequestItem(props: { GitHubData: GitHubData }) {
+  const pr : GitHubData = props.GitHubData;
+
+  return (
+    <div class="bg-white border border-black rounded mb-5 p-3 transition ease-in-out hover:scale-105 font-sans">
+      <a href={pr.pull_request.html_url} target="_blank">
+        <div>
+          <span>{pullRequestIcon(pr.state)}</span>{" "}
+          <span class="text-gray-500">
+            {pr.repository_url.replace("https://api.github.com/repos/", "")}
+          </span>{" "}
+          <span class="text-black">{pr.title}</span>
+        </div>
+      </a>
+    </div>
+  );
+}
--- routes/open-source.tsx
+++ routes/open-source.tsx
@@ -1,8 +1,49 @@
+import IconInfoSquare from "https://deno.land/x/tabler_icons_tsx@0.0.2/tsx/info-square.tsx"
+
+import { PageProps } from "$fresh/server.ts";
+
 import Head from "../components/Head.tsx";
 import SocialNav from "../components/SocialNav.tsx";
 import Navigation from "../components/Navigation.tsx";
+import PullRequestItem from "../components/PullRequestItem.tsx";
 
-export default function OpenSource() {
+import { Handlers } from "$fresh/server.ts";
+
+import { GitHubData, PullRequest, GitHubAPIQuery } from "../utils/GitHub.ts"
+
+export const handler: Handlers<GitHubData[]> = {
+  async GET(_req, ctx) {
+    const pullRequests = await getPullrequsts();
+    return ctx.render(pullRequests);
+  },
+};
+
+async function getPullrequsts(): Promise<GitHubData[]> {
+  const resp: Response = await fetch(
+    `https://api.github.com/search/issues?q=${GitHubAPIQuery}`,
+  );
+  const pull_request_items: { items: GitHubData[]} = await resp.json();
+
+  return pull_request_items.items.map((item: GitHubData): GitHubData => {
+    const pr_data: PullRequest = {
+      url: item.pull_request.url,
+      html_url: item.pull_request.html_url,
+      merged_at: item.pull_request.merged_at,
+    };
+
+    return {
+      url: item.url,
+      repository_url: item.repository_url,
+      title: item.title,
+      state: item.state,
+      pull_request: pr_data,
+    };
+  });
+}
+
+export default function OpenSource(props: PageProps<GitHubData[]>) {
+  const pullRequests = props.data;
+
   return (
     <div>
       <Head />
@@ -14,11 +55,16 @@ export default function OpenSource() {
           </div>
 
           <div class="col-span-3 sm:col-span-2">
-            <div
-              class="mt-5 text-5xl lg:text-6xl sm:text-left sm:mt-0"
-            >
-              <p>Coming soon!</p>
+            <div class="mt-5 text-5xl lg:text-4xl sm:text-left sm:mt-0">
+              <p>Open source!</p>
             </div>
+            <div class="mb-3 mt-3 pt-3 pb-3 rounded bg-banner">
+              <p class="mx-3">
+                <span> <IconInfoSquare class="inline"/> </span>
+                <span> This page queries GitHub's API and serves all pull requests I have created that are in public repositories. </span>
+              </p>
+            </div>
+            {pullRequests.map((githubData : GitHubData) => <PullRequestItem GitHubData={githubData} />)}
           </div>
         </div>
       </div>
