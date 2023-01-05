9330c1133226a7e1c39a1ad8887ba9e0306a8eff
Chris <38702971+Chrizpy@users.noreply.github.com>
2023-01-05 21:54:33 +0100

    Add `ga4` middleware
    Copying the one that was recently added in:
    https://github.com/denoland/fresh/pull/958

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,24 +3,26 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/about.tsx";
-import * as $1 from "./routes/conferences.tsx";
-import * as $2 from "./routes/cv.tsx";
-import * as $3 from "./routes/index.tsx";
-import * as $4 from "./routes/my-projects.tsx";
-import * as $5 from "./routes/open-source.tsx";
-import * as $6 from "./routes/tech-used.tsx";
+import * as $0 from "./routes/_middleware.tsx";
+import * as $1 from "./routes/about.tsx";
+import * as $2 from "./routes/conferences.tsx";
+import * as $3 from "./routes/cv.tsx";
+import * as $4 from "./routes/index.tsx";
+import * as $5 from "./routes/my-projects.tsx";
+import * as $6 from "./routes/open-source.tsx";
+import * as $7 from "./routes/tech-used.tsx";
 import * as $$0 from "./islands/CollapseButton.tsx";
 
 const manifest = {
   routes: {
-    "./routes/about.tsx": $0,
-    "./routes/conferences.tsx": $1,
-    "./routes/cv.tsx": $2,
-    "./routes/index.tsx": $3,
-    "./routes/my-projects.tsx": $4,
-    "./routes/open-source.tsx": $5,
-    "./routes/tech-used.tsx": $6,
+    "./routes/_middleware.tsx": $0,
+    "./routes/about.tsx": $1,
+    "./routes/conferences.tsx": $2,
+    "./routes/cv.tsx": $3,
+    "./routes/index.tsx": $4,
+    "./routes/my-projects.tsx": $5,
+    "./routes/open-source.tsx": $6,
+    "./routes/tech-used.tsx": $7,
   },
   islands: {
     "./islands/CollapseButton.tsx": $$0,
