d51bcc5b669da2d92fcbd4057e785e1e746bda0d
Chris <38702971+Chrizpy@users.noreply.github.com>
2023-06-03 12:23:57 +0200

    Add simple footer

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div>
+      <div class="text-gray-600 pt-5 pb-5 text-sm text-center">
+        Copyright © {new Date().getFullYear()} Christoffer Akouri
+      </div>
+    </div>
+  );
+}
