b1d325502e1ddbb8dbe44530ce4687cae4fd9cd6
Chris <christofferhaj@gmail.com>
2023-09-16 16:28:27 +0200

    Refactor CvCard component
    Use the Title component instead.
    Change the styling a bit.

--- components/CvCard.tsx
+++ components/CvCard.tsx
@@ -1,4 +1,5 @@
 import { ComponentChildren } from "preact";
+import Title from "./Title.tsx";
 
 interface CvProps {
   start: string;
@@ -11,29 +12,29 @@ interface CvProps {
 
 export default function CvCard(props: CvProps) {
   return (
-    <div class="mt-5 grid border-2 border-dashed border-black rounded-lg divide-y-2 divide-dashed divide-black transition ease-in-out hover:scale-95 tablet:grid-cols-4 tablet:divide-y-0 tablet:divide-x-2">
-      <div class="p-3 grid grid-cols-3 tablet:grid-cols-none">
-        <p class="text-md col-span-2 tablet:col-span-none">
-          {props.start} - {props.end}
+    <div class="mt-5 grid border-2 border-dashed border-gray-400 rounded-lg divide-y-2 divide-dashed divide-gray-400 transition ease-in-out hover:scale-95 tablet:grid-cols-4 tablet:divide-y-0 tablet:divide-x-2">
+      <div class="p-5 grid grid-cols-3 tablet:grid-cols-none">
+        <p class="text-sm col-span-2 tablet:col-span-1">
+          {props.start + " - " + props.end}
         </p>
 
         <img
           src={props.image}
-          class="p-5 mx-auto"
+          class="p-5"
         >
         </img>
       </div>
 
-      <div class="p-3 tablet:col-span-3">
-        <p class="text-4xl">
+      <div class="p-5 tablet:col-span-3">
+        <Title headerStyle="h2">
           {props.title}
-        </p>
+        </Title>
 
-        <p class="text-xl mb-5">
+        <p>
           {props.subTitle}
         </p>
 
-        <p class="text-xl">
+        <p class="mt-5">
           {props.children}
         </p>
       </div>
